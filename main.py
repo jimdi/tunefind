2ec78b6221f98c437c588d06c635e329733858f3
Jim_Di <jimdi@taximaxim.ru>
2017-04-28 23:35:49 +0500

    fix

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 import json
 import argparse
 
+
 def geturlorjson(url, fname):
     if os.path.exists(fname):
         with open(fname, 'r', encoding='utf-8') as f:
@@ -14,6 +15,7 @@ def geturlorjson(url, fname):
         with open(fname, 'w', encoding='utf-8') as f:
             f.write(json.dumps(_json))
     return _json
+
 
 parser = argparse.ArgumentParser(description='Parse OST for TV shows from tunefind')
 parser.add_argument('showname', metavar='showname', type=str, help='show name')
@@ -38,23 +40,25 @@ season_cnt = int(len(_json['seasons']))
 
 fout = open('result/' + str(showname) + '.md', 'w', encoding='utf-8')
 
-for x in range(1, season_cnt):
+for x in range(1, season_cnt+1):
     fout.write('# Season %d #\r\n' % x)
 
-    season_url = 'http://www.tunefind.com/api/frontend/show/%s/season/%s?fields=episodes,theme-song,music-supervisors&metatags=1' % (showname, x)
+    season_url = 'http://www.tunefind.com/api/frontend/show/%s/season/%s?fields=episodes,theme-song,music-supervisors&metatags=1' % (
+    showname, x)
     _json = geturlorjson(season_url, 'cache/%s_s%s.json' % (showname, x))
 
-    for e in _json['episodes']:
-        fout.write('## ==> S%0.2dE%0.2d - %s.txt <== ##\r\n' % (int(x), int(e['number']), e['name'].strip()))
+    if _json:
+        for e in _json['episodes']:
+            fout.write('## ==> S%0.2dE%0.2d - %s.txt <== ##\r\n' % (int(x), int(e['number']), e['name'].strip()))
 
-        episode_url = 'http://www.tunefind.com/api/frontend/episode/%s?fields=song-events,questions' % e['id']
-        _json = geturlorjson(episode_url, 'cache/%s.json' % (e['id']))
+            episode_url = 'http://www.tunefind.com/api/frontend/episode/%s?fields=song-events,questions' % e['id']
+            _json = geturlorjson(episode_url, 'cache/%s.json' % (e['id']))
 
-        for s in _json['song_events']:
-            fout.write('* Song: %s - %s\r\n' % (s['song']['artist']['name'].strip(), s['song']['name'].strip()))
-            if s['song']['album']:
-                fout.write('  Album: %s\r\n' % s['song']['album'].strip())
-            if s['description']:
-                fout.write('  Description: %s\r\n' % s['description'].strip().replace('\r', '').replace('\n', ''))
+            for s in _json['song_events']:
+                fout.write('* Song: %s - %s\r\n' % (s['song']['artist']['name'].strip(), s['song']['name'].strip()))
+                if s['song']['album']:
+                    fout.write('  Album: %s\r\n' % s['song']['album'].strip())
+                if s['description']:
+                    fout.write('  Description: %s\r\n' % s['description'].strip().replace('\r', '').replace('\n', ''))
 
 fout.close()
